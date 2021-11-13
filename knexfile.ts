5c7cb05449f9a6920658abe4f3e934b2164867a3
Knut Hühne <knut@k-nut.eu>
2021-11-13 11:05:21 +0100

    Add SSL config for kenx connection

--- knexfile.ts
+++ knexfile.ts
@@ -3,7 +3,10 @@ import { Knex } from "knex";
 
 const config: Knex.Config = {
   client: "pg",
-  connection: process.env.DATABASE_URL,
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: process.env.NODE_ENV === "production",
+  },
   pool: {
     min: 2,
     max: 10,
