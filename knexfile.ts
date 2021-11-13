632bd314a2a1c7857cb96264e0f6917992610080
Knut Hühne <knut@k-nut.eu>
2021-11-13 11:12:50 +0100

    Allow self signed Postgres certificates

--- knexfile.ts
+++ knexfile.ts
@@ -5,7 +5,10 @@ const config: Knex.Config = {
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    ssl: process.env.NODE_ENV === "production",
+    ssl:
+      process.env.NODE_ENV === "production"
+        ? { rejectUnauthorized: false } // allow self-signed certificate for Heroku/AWS
+        : false, // if we run locally, we don't want SSL at all
   },
   pool: {
     min: 2,
