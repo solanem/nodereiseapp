88b09e94fc4a4d819446a48d35c45216e21ad078
ViktorAttol <j.kolesnyk@gmx.de>
2021-12-19 18:07:06 +0100

    Journeyservice added

---
+++ services/JourneyService.ts
@@ -0,0 +1,48 @@
+import crypto from "crypto";
+import { Knex } from "knex";
+
+type Journey = {
+    name: string;
+    land: string;
+    startDate: Date;
+    endDate: Date;
+};
+
+type SavedJourney = Journey & {
+    id: string;
+};
+
+class JourneyService {
+    journeys: SavedJourney[] = [];
+    private knex: Knex;
+
+    constructor(knex: Knex) {
+        this.knex = knex;
+    }
+
+    async getAllJourneys(): Promise<Journey[]>{
+        return this.knex("journeys");
+    }
+
+    async addJourney(journey: Journey): Promise<SavedJourney> {
+        const newExpense = {
+            ...journey,
+            id: crypto.randomUUID(),
+        };
+        await this.knex("journeys").insert(newExpense);
+        return newExpense;
+    }
+
+    async editJourney(journey: Journey): Promise<void> {
+        await this.knex("journeys").where({ name: journey.name }).update(journey);
+    }
+
+    async deleteJourney(uuid: string): Promise<void> {
+        await this.knex("journeys").where({ id: uuid }).delete();
+    }
+}
+
+
+
+
+export default JourneyService;
